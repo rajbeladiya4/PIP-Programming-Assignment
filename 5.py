5885ccd0126fc902857abc8da302af7beb02100b
Raj Beladiya <102737656+beladiyaraj@users.noreply.github.com>
2022-04-13 10:27:36 +0530

    5th

--- 5.py
+++ 5.py
@@ -1,5 +1,17 @@
 # Explain about the different types of Exceptions in Python with suitable example.
-# 
+#
 # 20CE003_RAJ BELADIYA
 # https://github.com/rajbeladiya4/PIP-Programming-Assignment.git
-
+# 
+# Exceptions can either belong to the in -built errors/exceptions or have custom exceptions. Some of the common in -built exceptions are as follows:
+# 
+# 1. ZeroDivisionError
+# 2. NameError
+# 3. IndentationError
+# 4. IOError
+#
+# Zero division error
+a = int(input("Enter numerator: "))
+b = int(input("Enter denominator: "))
+print("a/b results in : ")
+print(a/b)
